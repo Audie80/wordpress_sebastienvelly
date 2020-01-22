cd6efbc256b1eabc11b10c8131af0a2a5c49a921
Audie <audie.menut@gmail.com>
2020-01-22 19:45:58 +0100

    gestion des background-images de l'animation par WP et le post-type 'image'

--- aside.php
+++ aside.php
@@ -7,11 +7,57 @@
 ?>
 
 <aside class="colonne-gauche">
-	<!-- image en arrière-plan dans le css -->
-	<div id="background-image-1"></div>
-	<div id="background-image-2"></div>
-	<div id="background-image-3"></div>
-	<div id="background-image-4"></div>
+
+	<!-- animation d'images en arrière-plan dans le css -->
+
+	<?php
+		$args = array(
+			'post_type' => 'image',
+			'name' => 'image-1'
+		);
+		$the_query = new WP_Query($args);
+	?>
+	<?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div id="background-image-1" style="--bg-image: url('<?php the_post_thumbnail_url('full'); ?>');"></div>
+	<?php endwhile; ?>
+
+	<?php
+		$args = array(
+			'post_type' => 'image',
+			'name' => 'image-2'
+		);
+		$the_query = new WP_Query($args);
+	?>
+	<?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div id="background-image-2" style="--bg-image: url('<?php the_post_thumbnail_url('full'); ?>');"></div>
+	<?php endwhile; ?>
+
+	<?php
+		$args = array(
+			'post_type' => 'image',
+			'name' => 'image-3'
+		);
+		$the_query = new WP_Query($args);
+	?>
+	<?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div id="background-image-3" style="--bg-image: url('<?php the_post_thumbnail_url('full'); ?>');"></div>
+	<?php endwhile; ?>
+		
+	<?php
+		$args = array(
+			'post_type' => 'image',
+			'name' => 'image-4'
+		);
+		$the_query = new WP_Query($args);
+	?>
+	<?php while ($the_query->have_posts() ) : $the_query->the_post(); ?>
+		<div id="background-image-4" style="--bg-image: url('<?php the_post_thumbnail_url('full'); ?>');"></div>
+	<?php endwhile; ?>
+
+	<?php
+		/* Restore original Post Data */
+		wp_reset_postdata();
+	?>
 
 	<?php if ( is_page_template( 'page.php' ) ) {?>
 		<!-- div apparaît seulement en hover, voir dans le css (seulement sur la page d'accueil) -->
